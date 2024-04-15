1215c6335d861ee7c9fdfd32649b991ada5db120
Stefanatti <stekots@gmail.com>
2024-04-15 17:29:15 +0300

    themeSwitcher updated

--- Client/src/Components/ThemeSwitcher.js
+++ Client/src/Components/ThemeSwitcher.js
@@ -3,44 +3,52 @@ import { useDispatch } from "react-redux";
 import { themeSwich } from "../Features/theme";
 
 const ThemeSwitcher = ({ theme, setTheme }) => {
-  const [switcher, setSwitcher] = useState(false);
-  const [moveBall, setMoveBall] = useState();
+  const [moveBall, setMoveBall] = useState(0);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (localStorage.getItem("theme")) {
-      console.log(localStorage.getItem("theme"));
-      setTheme(localStorage.getItem("theme"));
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      console.log(storedTheme);
+      setTheme(storedTheme);
+      switch (storedTheme) {
+        case "blue-theme":
+          setMoveBall(27);
+          break;
+        case "purple-theme":
+          setMoveBall(52);
+          break;
+        default:
+          setMoveBall(2);
+      }
     }
   }, []);
+
+  const handleThemeChange = (newTheme, ballPosition) => {
+    setMoveBall(ballPosition);
+    dispatch(themeSwich({ theme: newTheme }));
+    localStorage.setItem("theme", newTheme);
+  };
+
   return (
     <div className="switch">
       <div className="switch-radios">
         <input
-          onChange={() => {
-            setMoveBall(3);
-            dispatch(themeSwich({ theme: "" }));
-          }}
+          onChange={() => handleThemeChange("", 2)}
           type="radio"
           name="theme-type"
           value="one"
           id="first"
         />
         <input
-          onClick={() => {
-            setMoveBall(27);
-            dispatch(themeSwich({ theme: "blue-theme" }));
-          }}
+          onChange={() => handleThemeChange("blue-theme", 27)}
           type="radio"
           name="theme-type"
           value="two"
           id="second"
         />
         <input
-          onChange={() => {
-            setMoveBall(52);
-            dispatch(themeSwich({ theme: "purple-theme" }));
-          }}
+          onChange={() => handleThemeChange("purple-theme", 52)}
           type="radio"
           name="theme-type"
           value="three"

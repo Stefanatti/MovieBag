5cf8b3f5b6a5599b3a310538da29d7d7e252564c
Stefanatti <stekots@gmail.com>
2024-09-13 16:08:54 +0300

    --

--- Client/src/Components/Main.js
+++ Client/src/Components/Main.js
@@ -15,7 +15,7 @@ const Main = ({ user }) => {
   const navigate = useNavigate();
 
   const [openHaveToSignupModal, setOpenHaveToSignupModal] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const StyledTitleTypography = styled(Typography)(
--- Client/src/Features/showsSlice.js
+++ Client/src/Features/showsSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const movieApi = createApi({
   reducerPath: "movieApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3636",
+    baseUrl: "https://moviebag-1bhe.onrender.com",
   }),
   endpoints: (builder) => ({
     getPopularMovies: builder.query({

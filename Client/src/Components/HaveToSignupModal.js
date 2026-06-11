08580f195d7460a640da7345ef3b59ab78520020
Stefanos Kotsios <s.kotsios@mad.gr>
2026-06-11 19:14:59 +0300

    modals updated

--- Client/src/Components/HaveToSignupModal.js
+++ Client/src/Components/HaveToSignupModal.js
@@ -1,17 +1,130 @@
-const HaveToSignupModal = ({ open, onClose }) => {
-  if (!open) return null;
-  return (
-    <div className="modalOverlay" onClick={onClose}>
-      <div className="modalContainer">
-        <img
-          className="modal-poster"
-          src={require("../images/strain_lighthouse.jpg")}
-          alt="Sign up required"
-        />
+import {
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button,
+  Box,
+  CardMedia,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
-        <h3 class="title">You have to sign up first! </h3>
-      </div>
-    </div>
+const HaveToSignupModal = ({ open, onClose }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: {
+          borderRadius: "14px",
+          background: "var(--cards-color)",
+          border: "2px solid var(--basic-color)",
+          boxShadow: "0 16px 48px rgba(0,0,0,0.6)",
+          maxWidth: "420px",
+          width: "90%",
+          overflow: "hidden",
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="220"
+        image={require("../images/strain_lighthouse.jpg")}
+        alt="Sign up required"
+        sx={{ objectFit: "cover" }}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: -3.5,
+        }}
+      >
+        <Box
+          sx={{
+            width: 56,
+            height: 56,
+            borderRadius: "50%",
+            backgroundColor: "var(--basic-color)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            boxShadow: "0 4px 16px rgba(0,0,0,0.4)",
+            border: "3px solid var(--table-background-color)",
+          }}
+        >
+          <LockOutlinedIcon sx={{ color: "#fff", fontSize: 28 }} />
+        </Box>
+      </Box>
+      <DialogContent sx={{ textAlign: "center", pt: 2, pb: 1 }}>
+        <Typography
+          variant="h6"
+          sx={{
+            color: "var(--basic-color)",
+            fontFamily: "Limelight",
+            fontWeight: 600,
+            mb: 0.5,
+          }}
+        >
+          Sign Up Required
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "var(--table-text-color)", opacity: 0.8 }}
+        >
+          You need an account to save movies and create watchlists.
+        </Typography>
+      </DialogContent>
+      <DialogActions
+        sx={{
+          justifyContent: "center",
+          gap: 1.5,
+          pb: 2.5,
+          px: 3,
+        }}
+      >
+        <Button
+          onClick={onClose}
+          variant="outlined"
+          sx={{
+            borderColor: "var(--basic-color)",
+            color: "var(--basic-color)",
+            borderRadius: "8px",
+            textTransform: "none",
+            px: 3,
+            "&:hover": {
+              borderColor: "var(--hover-color)",
+              color: "var(--hover-color)",
+              background: "rgba(255,255,255,0.05)",
+            },
+          }}
+        >
+          Maybe Later
+        </Button>
+        <Button
+          onClick={() => {
+            onClose();
+            navigate("/signup");
+          }}
+          variant="contained"
+          sx={{
+            backgroundColor: "var(--basic-color)",
+            borderRadius: "8px",
+            textTransform: "none",
+            px: 3,
+            color: "#fff",
+            "&:hover": {
+              backgroundColor: "var(--hover-color)",
+            },
+          }}
+        >
+          Sign Up
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
--- Client/src/Components/LogoutModal.js
+++ Client/src/Components/LogoutModal.js
@@ -1,29 +1,16 @@
 import { logout } from "../Features/user";
 import { useDispatch } from "react-redux";
-
 import * as React from "react";
 import {
-  Card,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Typography,
   Button,
-  CardHeader,
-  Modal,
+  Box,
   CardMedia,
-  CardActions,
 } from "@mui/material";
-
-const style = {
-  position: "absolute",
-  backgroundColor: "red",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "40%",
-  bgcolor: "rgb(39 39 38)",
-  border: "2px solid #000",
-  borderRadius: "10px",
-  boxShadow: 24,
-  p: 4,
-};
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const LogoutModal = ({ open, onClose }) => {
   const dispatch = useDispatch();
@@ -31,49 +18,122 @@ const LogoutModal = ({ open, onClose }) => {
   const logOut = () => {
     if (localStorage.getItem("token")) {
       localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("id");
       dispatch(logout());
     }
+    onClose();
   };
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={onClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: {
+          borderRadius: "14px",
+          background: "var(--cards-color)",
+          border: "2px solid var(--basic-color)",
+          boxShadow: "0 16px 48px rgba(0,0,0,0.6)",
+          maxWidth: "420px",
+          width: "90%",
+          overflow: "hidden",
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="220"
+        image={require("../images/maxresdefault.jpg")}
+        alt="Logout"
+        sx={{ objectFit: "cover" }}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: -3.5,
+        }}
       >
-        <Card sx={style}>
-          <CardHeader
-            sx={{ textAlign: "center", color: "var(--basic-color)" }}
-            title="Are you sure you want to log out?"
-          />
-          <CardMedia
-            component="img"
-            height="254"
-            image={require("../images/maxresdefault.jpg")}
-            alt="Paella dish"
-          />
-          <CardActions
-            sx={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <Button variant="contained" size="small" onClick={onClose}>
-              Stay
-            </Button>
-            <Button
-              variant="contained"
-              color="error"
-              size="small"
-              onClick={() => {
-                logOut();
-              }}
-            >
-              Leave
-            </Button>
-          </CardActions>
-        </Card>
-      </Modal>
-    </div>
+        <Box
+          sx={{
+            width: 56,
+            height: 56,
+            borderRadius: "50%",
+            backgroundColor: "var(--basic-color)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            boxShadow: "0 4px 16px rgba(0,0,0,0.4)",
+            border: "3px solid var(--table-background-color)",
+          }}
+        >
+          <LogoutIcon sx={{ color: "#fff", fontSize: 26 }} />
+        </Box>
+      </Box>
+      <DialogContent sx={{ textAlign: "center", pt: 2, pb: 1 }}>
+        <Typography
+          variant="h6"
+          sx={{
+            color: "var(--basic-color)",
+            fontFamily: "Limelight",
+            fontWeight: 600,
+            mb: 0.5,
+          }}
+        >
+          Leaving so soon?
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "var(--table-text-color)", opacity: 0.8 }}
+        >
+          Are you sure you want to log out of your account?
+        </Typography>
+      </DialogContent>
+      <DialogActions
+        sx={{
+          justifyContent: "center",
+          gap: 1.5,
+          pb: 2.5,
+          px: 3,
+        }}
+      >
+        <Button
+          onClick={onClose}
+          variant="outlined"
+          sx={{
+            borderColor: "var(--basic-color)",
+            color: "var(--basic-color)",
+            borderRadius: "8px",
+            textTransform: "none",
+            px: 3,
+            "&:hover": {
+              borderColor: "var(--hover-color)",
+              color: "var(--hover-color)",
+              background: "rgba(255,255,255,0.05)",
+            },
+          }}
+        >
+          Stay
+        </Button>
+        <Button
+          onClick={logOut}
+          variant="contained"
+          sx={{
+            backgroundColor: "var(--basic-color)",
+            borderRadius: "8px",
+            textTransform: "none",
+            px: 3,
+            color: "#fff",
+            "&:hover": {
+              backgroundColor: "var(--hover-color)",
+            },
+          }}
+        >
+          Log Out
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
